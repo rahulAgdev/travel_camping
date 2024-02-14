a8be335d2df4ee3aa044f8b029e16dd11eab0671
Rahul Agarwal <rahulagdev@gmail.com>
2024-02-14 06:55:57 +0530

    Styling GetApp section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,17 +4,18 @@ type ButtonProps = {
     type: 'button' | 'submit';
     title: string;
     icon?: string;
-    variant: string
+    variant: string;
+    full?: boolean
 }
-const Button = ({type, title, icon, variant}: ButtonProps) => {
-  return <button type={type} className={`flex-center gap-3 rounded-full border ${variant}`}>
+const Button = ({type, title, icon, variant, full}: ButtonProps) => {
+  return <button type={type} className={`flex-center gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>
     {icon && <Image 
         src={icon}
         alt={title}
         width={24}
         height={24}
     />}
-    <label className="bold-15 whitespace-nowrap">
+    <label className="bold-15 whitespace-nowrap cursor-pointer">
         {title}
     </label>
   </button>;
